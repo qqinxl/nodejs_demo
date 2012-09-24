bb3a02058731262fa2eb52ee9242151744566441
qqinxl <qqinxl@gmail.com>
2012-09-24 17:27:29 +0900

    removed some files

--- bin/chatroom/chat.01.js
+++
@@ -1,8 +0,0 @@
-var net=require('net');
-var chatServer=net.createServer();
-chatServer.on('connection',function(client){
-    client.write('hi!\n');
-    client.write('bye!\n');
-    client.end();
-})
-chatServer.listen(9001);
